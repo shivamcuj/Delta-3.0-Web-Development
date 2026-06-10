2b4e25e33469ac2d68151ba92b1b8e70fc4a3891
Shivam Kumar <shivamjnv2305@gmail.com>
2026-06-10 18:22:42 +0530

    Create Q3.js

---
+++ Day 15/Q3.js
@@ -0,0 +1,15 @@
+let quarter = 3;
+switch(quarter){
+    case 1:
+        console.log("January, February, March");
+        break;
+    case 2:
+        console.log("April, May, June");
+        break;
+    case 3:
+        console.log("July, August, September");
+        break;
+    case 4:
+        console.log("October, November, December");
+        break;
+}
