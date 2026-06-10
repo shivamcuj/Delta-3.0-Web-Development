181aae6a0999f46710573f32ae29ec99332ab386
Shivam Kumar <shivamjnv2305@gmail.com>
2026-06-10 18:47:28 +0530

    Update Q2.js

--- Day 17/Q2.js
+++ Day 17/Q2.js
@@ -4,3 +4,4 @@
 let number = 22936829472005;
 
 console.log(number.toString().length); // Output: 14
+// Explanation: The `toString()` method converts the number into a string, and then the `length` property counts the number of characters in that string, which corresponds to the number of digits in the original number.
