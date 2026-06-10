44982db6fb6484ff821dbc2d0abac853c59d1b30
Shivam Kumar <shivamjnv2305@gmail.com>
2026-06-10 18:46:47 +0530

    code added

---
+++ Day 17/Q2.js
@@ -0,0 +1,6 @@
+// Write a JS program to find the number of digits in a number.
+// Example: if number=287152, count=6
+
+let number = 22936829472005;
+
+console.log(number.toString().length); // Output: 14
