124b4f1aa352bfef2dc7eade8177a83cdf710d82
Shivam Kumar <shivamjnv2305@gmail.com>
2026-06-10 18:23:21 +0530

    Create Q5.js

---
+++ Day 15/Q5.js
@@ -0,0 +1,11 @@
+let A = 23;
+let B = 21;
+let C = 99;
+
+if(A > B && A > C){
+    console.log("A is the greatest");
+} else if(B > C&& B > A ){
+    console.log("B is the greatest");
+}else{
+    console.log("C is the greatest");
+}
